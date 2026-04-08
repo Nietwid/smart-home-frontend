9b6018879b7419d331ec3902eb03a80967c1f470
Dawid Franczak <daw.franczak@gmail.com>
2026-04-08 20:51:51 +0200

    refine Rc522 widget UI and fix name positioning

--- src/components/Widgets/Rc522/Rc522.tsx
+++ src/components/Widgets/Rc522/Rc522.tsx
@@ -1,18 +1,61 @@
 import BaseWidget from "../BaseWidget/BaseWidget.tsx";
 import {IRc522Widget} from "../../../interfaces/Widgets/IRc522.ts";
-
+import GearIcon from '@rsuite/icons/Gear';
 import CardManager from "./CardManager/CardManager.tsx";
-import {useState} from "react";
+import {useState, useMemo} from "react";
 import {Button} from "rsuite";
-
+import DEVICE_PENDING from "../../../enums/device_pending.ts"
+import useRfidCardQuery from "../../../hooks/queries/useRfidCardQuery.tsx";
+import styles from "./Rc522.module.css";
+import {useTranslation} from "react-i18next";
 
 export default function Rc522({id, state, config, pending}:IRc522Widget){
+    const { t } = useTranslation();
     const [showCardManager, setShowCardManager] = useState(false);
+    const isAddingCard = pending.includes(DEVICE_PENDING.ADD_TAG);
+    const {cards} = useRfidCardQuery(id)
+
+    const lastUsedCard = useMemo(() => {
+        if (!cards || !cards.length) return null;
+        return [...cards].sort((a, b) =>
+            new Date(b.last_used).getTime() - new Date(a.last_used).getTime()
+        )[0];
+    }, [cards]);
+
+    const formatTime = (dateString: string) => {
+        const date = new Date(dateString);
+        return date.toLocaleTimeString([], {day:'2-digit',month:'2-digit',year:'2-digit', hour: '2-digit', minute: '2-digit' });
+    };
     return (
         <BaseWidget name={config?.name} size="md">
-            <Button onClick={()=>setShowCardManager(true)}>CARD</Button>
+            <div className={styles.container}>
+                <div className={styles.infoSection}>
+                    {lastUsedCard && (
+                        <>
+                            <span className={styles.lastCardName}>{lastUsedCard.name}</span>
+                            <span className={styles.lastAccess}>{formatTime(lastUsedCard.last_used)}</span>
+                        </>
+                    )}
+                    {!lastUsedCard && !isAddingCard && <span> {t("rfidCard.noCard")}</span>}
+                    {isAddingCard && <span className={styles.pairing}> {t("rfidCard.parsing")}</span>}
+                </div>
 
-            <CardManager id={id} pending={pending.includes("add_tag")} open={showCardManager} onClose={() => setShowCardManager(false)} />
+                <Button
+                    className={styles.managerButton}
+                    appearance="subtle"
+                    startIcon={<GearIcon />}
+                    onClick={() => setShowCardManager(true)}
+                >
+                    {t("rfidCard.cardManagerButton")}
+                </Button>
+
+            </div>
+            <CardManager
+                id={id}
+                pending={isAddingCard}
+                open={showCardManager}
+                onClose={() => setShowCardManager(false)}
+            />
         </BaseWidget>
     );
 }
---
+++ src/enums/device_pending.ts
@@ -0,0 +1,3 @@
+export default {
+    ADD_TAG : "add_tag"
+}
