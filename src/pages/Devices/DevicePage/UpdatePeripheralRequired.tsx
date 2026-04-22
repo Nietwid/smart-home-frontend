a96a4ee77a0e546eef26e7426abd8c372e5c6b2b
Dawid Franczak <daw.franczak@gmail.com>
2026-04-22 20:21:19 +0200

    add UpdatePeripheralRequired component with hero-state UI

--- src/pages/Devices/DevicePage/UpdatePeripheralRequired.tsx
+++ src/pages/Devices/DevicePage/UpdatePeripheralRequired.tsx
@@ -1,10 +1,42 @@
-import {Button} from "rsuite";
+import { Button } from "rsuite";
 import SyncType from "../../../constant/syncType.ts";
+import NoticeIcon from '@rsuite/icons/Notice';
+import styles from './UpdatePeripheralRequired.module.css';
+import {useTranslation} from "react-i18next";
 
 interface IProps {
-    onClick: (type:SyncType) => void;
+    onClick: (type: SyncType) => void;
+    pending: boolean;
 }
 
-export default function UpdatePeripheralRequired({onClick}:IProps){
-    return <Button onClick={()=>onClick(SyncType.PERIPHERAL)}>UPDATE</Button>
+export default function UpdatePeripheralRequired({ onClick, pending }: IProps) {
+    const {t} = useTranslation();
+    return (
+        <div className={styles.updateContainer}>
+            <div className={styles.updateContent}>
+                <div className={styles.pulseIconWrapper}>
+                    <NoticeIcon style={{ fontSize: 60, color: '#ff922b' }} />
+                </div>
+
+                <div>
+                    <h2 className={styles.updateTitle}>
+                        {t("updatePeripheralRequired.syncRequired")}
+                    </h2>
+                    <p className={styles.updateDescription}>
+                        {t("updatePeripheralRequired.message")}
+                    </p>
+                </div>
+
+                <Button
+                    appearance="primary"
+                    color="orange"
+                    className={styles.updateButton}
+                    loading={pending}
+                    onClick={() => onClick(SyncType.PERIPHERAL)}
+                >
+                    {pending ? t("updatePeripheralRequired.buttonUpdating"):t("updatePeripheralRequired.buttonStart")}
+                </Button>
+            </div>
+        </div>
+    );
 }
